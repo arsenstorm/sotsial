969b70dcd3a1d5ab70406be40961ac90d1a76d60
Arsen Shkrumelyak <arsen@nerverift.com>
2025-04-06 21:29:22 +0100

    change layout

--- apps/web/src/app/(marketing)/layout.tsx
+++ apps/web/src/app/(marketing)/layout.tsx
@@ -10,19 +10,6 @@ export default function MarketingLayout({
 }>) {
 	return (
 		<div className="min-h-screen bg-zinc-100 dark:bg-zinc-900 relative">
-			<div className="fixed top-0 left-0 right-0 bottom-16 w-full z-0">
-				<div className="h-full w-full max-w-7xl mx-auto rounded-b-2xl sm:rounded-b-4xl bg-white dark:bg-black" />
-			</div>
-
-			<div className="relative z-[1] w-full max-w-7xl mx-auto px-4 sm:px-8">
-				<div className="bg-white dark:bg-black rounded-b-2xl sm:rounded-b-4xl relative z-[1]">
-					{children}
-				</div>
-				<div className="relative -mx-6">
-					<Footer simpleFooter className="w-full max-w-7xl mx-auto" />
-				</div>
-			</div>
-
 			<div className="sticky bottom-0 w-full bg-zinc-100 dark:bg-zinc-900 z-10">
 				<header className="h-16 flex flex-row items-center justify-between w-full max-w-7xl mx-auto">
 					<div className="px-4 flex flex-row justify-between items-center py-4 font-semibold relative z-10 w-full">
@@ -34,7 +21,7 @@ export default function MarketingLayout({
 							Sotsial
 						</TextLink>
 						<div className="flex flex-row gap-x-6">
-							<TextLink href="/continue" className="hidden md:inline-flex">
+							<TextLink href="/continue">
 								Dashboard
 							</TextLink>
 							<TextLink href="/docs" className="hidden md:inline-flex">
@@ -47,6 +34,12 @@ export default function MarketingLayout({
 					</div>
 				</header>
 			</div>
+
+			<div className="relative z-[1] w-full max-w-7xl mx-auto px-4 sm:px-8 bg-white dark:bg-black rounded-4xl">
+				{children}
+			</div>
+
+			<Footer className="w-full max-w-7xl mx-auto mt-6" />
 		</div>
 	);
 }
